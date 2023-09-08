1af70b64977fc1304dfbbd053b0960e28e61af50
Artuqu <percikartur@gmail.com>
2023-09-08 14:16:05 +0200

    PassingCars second solution

--- src/main/java/prefixSums/PassingCars.java
+++ src/main/java/prefixSums/PassingCars.java
@@ -4,7 +4,7 @@ public class PassingCars {
 
     public int solution(int[] A) {
         int eastCars = 0;
-        long totalCars = 0;
+        int totalCars = 0;
         for (int i = 0; i < A.length; i++) {
             if (A[i] == 0) {
                 eastCars++;
@@ -15,14 +15,26 @@ public class PassingCars {
         if (totalCars > 1000000000) {
             return -1;
         }
-        return (int) totalCars;
+        return totalCars;
     }
+
+    public int solution2(int[] A) {
+        int[] suffixSum = new int[A.length + 1];
+        int count = 0;
+        for (int i = A.length - 1; i >= 0; i--) {
+            suffixSum[i] = A[i] + suffixSum[i + 1];
+            if (A[i] == 0) count += suffixSum[i];
+            if (count > 1000000000) return -1;
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
         int[] A = {0, 1, 0, 1, 1};
         int[] A1 = {0};
-        System.out.println(new PassingCars().solution(A1));
-        System.out.println(new PassingCars().solution(A));
+        System.out.println(new PassingCars().solution2(A1));
+        System.out.println(new PassingCars().solution2(A));
 
     }
 }
