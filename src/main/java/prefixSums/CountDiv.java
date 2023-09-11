af1c5f8c403679c61d5fb42509a05beb4dd5d4c4
Artuqu <percikartur@gmail.com>
2023-09-11 20:23:23 +0200

    CountDiv second solution

--- src/main/java/prefixSums/CountDiv.java
+++ src/main/java/prefixSums/CountDiv.java
@@ -20,10 +20,16 @@ public class CountDiv {
         return lastDivider / K - firstDivider / K + 1;
     }
 
+    public int solution2(int A, int B, int K) {
+        double start = Math.ceil((double) A / K);
+        double end = Math.floor((double) B / K);
+        return (int) (end - start + 1);
+    }
+
 
     public static void main(String[] args) {
 
-        System.out.println(new CountDiv().solution(6, 11, 2));
-        System.out.println(new CountDiv().solution(6, 6, 2));
+        System.out.println(new CountDiv().solution2(6, 11, 2));
+        System.out.println(new CountDiv().solution2(6, 6, 2));
     }
 }
