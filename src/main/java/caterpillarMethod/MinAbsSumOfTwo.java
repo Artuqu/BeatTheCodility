e8886234ffb130c1e796d84cbb2df3fc04401917
Artuqu <percikartur@gmail.com>
2023-09-16 19:51:10 +0200

    MinAbsSumOfTwo second solution

--- src/main/java/caterpillarMethod/MinAbsSumOfTwo.java
+++ src/main/java/caterpillarMethod/MinAbsSumOfTwo.java
@@ -24,10 +24,25 @@ public class MinAbsSumOfTwo {
         return minValue;
     }
 
+//    second solution
+
+    public int solution2(int[] A) {
+        Arrays.sort(A);
+        int minAbsSumOfTwo = Integer.MAX_VALUE;
+        int tail = 0;
+        int head = A.length - 1;
+        while (head >= tail) {
+            minAbsSumOfTwo = Math.min(minAbsSumOfTwo, Math.abs(A[tail] + A[head]));
+            if (A[tail] + A[head] < 0) tail++;
+            else head--;
+        }
+        return minAbsSumOfTwo;
+    }
+
     public static void main(String[] args) {
         int[] A = {1, 4, -3};
         int[] A1 = {-8, 4, 5, -10, 3};
-        System.out.println(new MinAbsSumOfTwo().solution(A1));
-        System.out.println(new MinAbsSumOfTwo().solution(A));
+        System.out.println(new MinAbsSumOfTwo().solution2(A1));
+        System.out.println(new MinAbsSumOfTwo().solution2(A));
     }
 }
