f923470c47d13bb200fede1ede2fc4d36bcbb327
Artuqu <percikartur@gmail.com>
2023-09-17 10:30:53 +0200

    MaxNonoverlappingSegments first solution

---
+++ src/main/java/greedyAlgorithms/MaxNonoverlappingSegments.java
@@ -0,0 +1,43 @@
+package greedyAlgorithms;
+
+import java.util.Stack;
+
+public class MaxNonoverlappingSegments {
+    public int solution(int[] A, int[] B) {
+        Stack<Segment> segmentStack = new Stack<>();
+        for (int i = 0; i < A.length; i++) {
+            Segment segment = new Segment(A[i], B[i]);
+            if (segmentStack.isEmpty() || segment.start > segmentStack.peek().end) {
+                segmentStack.push(segment);
+            }
+        }
+        return segmentStack.size();
+    }
+
+    class Segment {
+        private int start;
+        private int end;
+
+        public Segment(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 3, 7, 9, 9};
+        int[] B = {5, 6, 8, 9, 10};
+        int[] A1 = {};
+        int[] B1 = {};
+        System.out.println(new MaxNonoverlappingSegments().solution(A1, B1));
+        System.out.println(new MaxNonoverlappingSegments().solution(A, B));
+    }
+}
