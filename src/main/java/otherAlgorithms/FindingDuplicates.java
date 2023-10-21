54a6359f126c6c878eb6566656e303e75ae8c20e
Artuqu <percikartur@gmail.com>
2023-10-21 16:58:10 +0200

    Finding duplicates: printing only duplicates with bitset

--- src/main/java/otherAlgorithms/FindingDuplicates.java
+++ src/main/java/otherAlgorithms/FindingDuplicates.java
@@ -1,10 +1,12 @@
 package otherAlgorithms;
 
+import java.util.BitSet;
+
 public class FindingDuplicates {
 
 
     public void solution(int[] A) {
-        boolean[] withoutDuplicates = new boolean[3200];
+        boolean[] withoutDuplicates = new boolean[3201];
         for (int i = 0; i < A.length; i++) {
             withoutDuplicates[A[i]] = true;
         }
@@ -13,11 +15,25 @@ public class FindingDuplicates {
                 System.out.print(i + " ");
             }
         }
+        System.out.println();
+    }
+
+    public void solution2(int[] A) {
+        BitSet bits = new BitSet();
+        for (int i = 0; i < A.length; i++) {
+            int number = A[i];
+            if (bits.get(number)) {
+                System.out.println("Duplicate: " + number);
+            }
+            bits.set(number);
+        }
     }
 
     public static void main(String[] args) {
-        int[] A = {1, 5, 1, 7, 0, 9, 5,};
+        int[] A = {1, 5, 1, 7, 0, 9, 5, 3200};
 
         new FindingDuplicates().solution(A);
+        new FindingDuplicates().solution2(A);
+
     }
 }
