a7139a4b94c169a33aac1798522ee58c65a5752c
Artuqu <percikartur@gmail.com>
2023-10-22 21:16:47 +0200

    SquaresOfSortedArray O(n log n) solution

---
+++ src/main/java/otherAlgorithms/SquaresOfSortedArray.java
@@ -0,0 +1,28 @@
+package otherAlgorithms;
+
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+
+    //O(n log n) time complexity
+    public int[] solution(int[] A) {
+        if (A == null) {
+            return new int[]{};
+        }
+        int[] result = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            result[i] = A[i] * A[i];
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {-4, -1, 0, 3, 10};
+        int[] A1 = {-7, -3, 2, 3, 11};
+        int[] A2 = null;
+        System.out.println(Arrays.toString(new SquaresOfSortedArray().solution(A)));
+        System.out.println(Arrays.toString(new SquaresOfSortedArray().solution(A1)));
+        System.out.println(Arrays.toString(new SquaresOfSortedArray().solution(A2)));
+    }
+}
