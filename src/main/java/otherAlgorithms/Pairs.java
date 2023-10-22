cc0c14f6f70fa7b0a7e7ba24e872d542d3ac9475
Artuqu <percikartur@gmail.com>
2023-10-22 16:36:33 +0200

    Pairs second solution with while loop

--- src/main/java/otherAlgorithms/Pairs.java
+++ src/main/java/otherAlgorithms/Pairs.java
@@ -2,14 +2,18 @@ package otherAlgorithms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Pairs {
 
 
     public List<String> solution(int N, int[] A) {
-        List<String> pairs = new ArrayList<>();
+        if (A == null || A.length < 2) {
+            return Collections.emptyList();
+        }
         Arrays.sort(A);
+        List<String> pairs = new ArrayList<>();
         int leftPointer = 0;
         int rightPointer = A.length - 1;
         int sum = A[leftPointer] + A[rightPointer];
@@ -27,9 +31,33 @@ public class Pairs {
         return pairs;
     }
 
+    public List<String> solution2(int N, int[] A) {
+        if (A == null || A.length < 2) {
+            return Collections.emptyList();
+        }
+        Arrays.sort(A);
+        List<String> pairs = new ArrayList<>();
+        int leftPointer = 0;
+        int rightPointer = A.length - 1;
+        while (leftPointer < rightPointer) {
+            int sum = A[leftPointer] + A[rightPointer];
+            if (N > sum) leftPointer++;
+            else if (N < sum) rightPointer--;
+            else {
+                pairs.add("(" + A[leftPointer] + ", " + A[rightPointer] + ")");
+                leftPointer++;
+                rightPointer--;
+            }
+        }
+
+        return pairs;
+    }
+
     public static void main(String[] args) {
         int[] A = {2, 3, -1, 9, -5, 10};
-        System.out.println(new Pairs().solution(5, A));
+        int[] A1 = null;
+        System.out.println(new Pairs().solution(5, A1));
+        System.out.println(new Pairs().solution2(5, A));
 
     }
 }
