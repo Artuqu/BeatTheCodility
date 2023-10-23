da1e88647980910256023ab7f33bb632c830712b
Artuqu <percikartur@gmail.com>
2023-10-23 22:37:05 +0200

    ValidParentheses second solution

--- src/main/java/otherAlgorithms/ValidParentheses.java
+++ src/main/java/otherAlgorithms/ValidParentheses.java
@@ -1,11 +1,13 @@
 package otherAlgorithms;
 
+import java.util.Map;
 import java.util.Stack;
 
 public class ValidParentheses {
 
     public boolean solution(String s) {
         boolean result = true;
+        if (s == null || s.isEmpty()) return false;
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char bracket = s.charAt(i);
@@ -22,12 +24,31 @@ public class ValidParentheses {
         return result;
     }
 
+    Map<Character, Character> characters = Map.of(
+            ')', '(',
+            ']', '[',
+            '}', '{'
+    );
+
+    public boolean solution2(String s) {
+        if (s == null || s.isEmpty()) return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char input = s.charAt(i);
+            if (characters.containsValue(input)) stack.push(input);
+            else {
+                if (stack.isEmpty() || stack.pop() != characters.get(input)) return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String s = "()";
         String s1 = "()[]{}";
         String s2 = "(]";
-        System.out.println(new ValidParentheses().solution(s));
-        System.out.println(new ValidParentheses().solution(s1));
-        System.out.println(new ValidParentheses().solution(s2));
+        System.out.println(new ValidParentheses().solution2(s));
+        System.out.println(new ValidParentheses().solution2(s1));
+        System.out.println(new ValidParentheses().solution2(s2));
     }
 }
