c5ee3c667d92b82f6349426b2af3632a698bc3d5
Artuqu <percikartur@gmail.com>
2023-10-25 22:01:53 +0200

    ContainsDuplicate third solution O(n)

--- src/main/java/otherAlgorithms/ContainsDuplicate.java
+++ src/main/java/otherAlgorithms/ContainsDuplicate.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class ContainsDuplicate {
 
-//    O(n log (n))
+    //    O(n log (n))
     public boolean solution(int[] A) {
         if (A == null) return false;
         Arrays.sort(A);
@@ -20,9 +20,11 @@ public class ContainsDuplicate {
         }
         return false;
     }
-//    O(n) time complexity
+
+    //    O(n) time complexity
     public boolean solution2(int[] A) {
-    Set<Integer> cache = new HashSet<>();
+        if (A == null) return false;
+        Set<Integer> cache = new HashSet<>();
         for (int j : A) {
             if (cache.contains(j)) return true;
             else {
@@ -32,17 +34,28 @@ public class ContainsDuplicate {
         return false;
     }
 
+    public boolean solution3(int[] A) {
+        if (A == null) return false;
+        Set<Integer> cache = new HashSet<>();
+        for (int num : A) {
+            cache.add(num);
+        }
+        return A.length != cache.size();
+    }
+
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 1};
         int[] A1 = {1, 2, 3, 4};
         int[] A2 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
         int[] A3 = {1};
         int[] A4 = null;
-        System.out.println(new ContainsDuplicate().solution2(A));
-        System.out.println(new ContainsDuplicate().solution2(A1));
-        System.out.println(new ContainsDuplicate().solution2(A2));
-        System.out.println(new ContainsDuplicate().solution2(A3));
+        int[] A5 = {};
+        System.out.println(new ContainsDuplicate().solution3(A));
+        System.out.println(new ContainsDuplicate().solution3(A1));
+        System.out.println(new ContainsDuplicate().solution3(A2));
+        System.out.println(new ContainsDuplicate().solution3(A3));
         System.out.println(new ContainsDuplicate().solution2(A4));
+        System.out.println(new ContainsDuplicate().solution2(A5));
 
     }
 }
