1f83f3f32d1a3357bc0f3d362e28af763d781298
Artuqu <percikartur@gmail.com>
2023-10-26 23:04:37 +0200

    MissingInteger O(2n) solution

--- src/main/java/otherAlgorithms/MissingInteger.java
+++ src/main/java/otherAlgorithms/MissingInteger.java
@@ -1,6 +1,8 @@
 package otherAlgorithms;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MissingInteger {
 
@@ -13,12 +15,24 @@ public class MissingInteger {
         return A.length;
     }
 
+    //    O(2n)
+    public int solution2(int[] A) {
+        Set<Integer> cache = new HashSet<>();
+        for (int a : A) cache.add(a);
+        for (int i = 0; i < A.length + 1; i++) {
+            if (!cache.contains(i)) return i;
+        }
+        throw new RuntimeException("Not found!");
+    }
+
     public static void main(String[] args) {
         int[] A = {1, 2, 3};
         int[] A1 = {0, 1, 2};
         int[] A2 = {0, 5, 3, 4, 2};
-        System.out.println(new MissingInteger().solution(A));
-        System.out.println(new MissingInteger().solution(A1));
-        System.out.println(new MissingInteger().solution(A2));
+        int[] A3 = {};
+        System.out.println(new MissingInteger().solution2(A));
+        System.out.println(new MissingInteger().solution2(A1));
+        System.out.println(new MissingInteger().solution2(A2));
+        System.out.println(new MissingInteger().solution2(A3));
     }
 }
