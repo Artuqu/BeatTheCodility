ada507642a193f2d79da22c838834f5039d67444
Artuqu <percikartur@gmail.com>
2023-11-19 11:38:18 +0100

    LongestCommonPrefix scanning solution

--- src/main/java/otherAlgorithms/LongestCommonPrefix.java
+++ src/main/java/otherAlgorithms/LongestCommonPrefix.java
@@ -20,13 +20,34 @@ public class LongestCommonPrefix {
         return sb.toString();
     }
 
+    public String scanning(String[] strings) {
+        String lcp = strings[0];
+        for (int i = 1; i < strings.length; i++) {
+            lcp = commonPrefix(lcp, strings[i]);
+        }
+        return lcp;
+    }
+
+    private String commonPrefix(String s1, String s2) {
+        int min = Math.min(s1.length(), s2.length());
+        for (int i = 0; i < min; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) return s1.substring(0, i);
+        }
+        return s1.substring(0, min);
+    }
+
     public static void main(String[] args) {
         String[] strings1 = {"flat", "flower", "flow", "flight", "florida"};
         String[] strings2 = {"dog", "cat", "car"};
         String[] strings3 = {"can", "cat", "car"};
         System.out.println(new LongestCommonPrefix().solution(strings1));
+        System.out.println(new LongestCommonPrefix().scanning(strings1));
+
         System.out.println(new LongestCommonPrefix().solution(strings2));
+        System.out.println(new LongestCommonPrefix().scanning(strings2));
+
         System.out.println(new LongestCommonPrefix().solution(strings3));
+        System.out.println(new LongestCommonPrefix().scanning(strings3));
 
     }
 }
