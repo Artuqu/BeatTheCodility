2e39878ab83333ce51bac4f38120eb3197fdfa4a
Artuqu <percikartur@gmail.com>
2023-11-22 19:28:24 +0100

    NTreeHeight BFS second solution

--- src/main/java/otherAlgorithms/binarySearch/NTreeHeight.java
+++ src/main/java/otherAlgorithms/binarySearch/NTreeHeight.java
@@ -1,5 +1,10 @@
 package otherAlgorithms.binarySearch;
 
+
+import javafx.util.Pair;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 
 public class NTreeHeight {
@@ -12,6 +17,24 @@ public class NTreeHeight {
             height = Math.max(height, solution(node));
         }
         return height + 1;
+    }
+
+    //BFS
+    public int solution2(NTreeNode<Integer> root) {
+        Deque<Pair<NTreeNode<Integer>, Integer>> queue = new ArrayDeque<>();
+        NTreeNode<Integer> parent = root;
+        if (parent != null) queue.add(new Pair<>(parent, 1));
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            Pair<NTreeNode<Integer>, Integer> current = queue.poll();
+            parent = current.getKey();
+            int h = current.getValue();
+            if (parent != null) {
+                depth = Math.max(depth, h);
+                for (NTreeNode<Integer> node : parent.getChildren()) queue.add(new Pair<>(node, h + 1));
+            }
+        }
+        return depth;
     }
 
     public static void main(String[] args) {
@@ -32,5 +55,6 @@ public class NTreeHeight {
         node6.setChildren(List.of(node7, node8, node9));
 
         System.out.println(new NTreeHeight().solution(root));
+//        System.out.println(new NTreeHeight().solution2(root));
     }
 }
