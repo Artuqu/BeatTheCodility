9bb80724faefa78e90831f2e41eb97ba6e4ade11
Artuqu <percikartur@gmail.com>
2023-11-24 00:26:41 +0100

    Same Binary Trees DFS second solution

--- src/main/java/otherAlgorithms/binarySearch/SameBinaryTrees.java
+++ src/main/java/otherAlgorithms/binarySearch/SameBinaryTrees.java
@@ -7,6 +7,7 @@ public class SameBinaryTrees {
 
     public boolean bfsSolution(TreeNode<Integer> p, TreeNode<Integer> q) {
         if (p == null || q == null) return false;
+        if (p == null && q == null) return true;
         Queue<TreeNode<Integer>> queueP = new ArrayDeque<>();
         Queue<TreeNode<Integer>> queueQ = new ArrayDeque<>();
         queueP.add(p);
@@ -16,19 +17,25 @@ public class SameBinaryTrees {
             TreeNode<Integer> currP = queueP.poll();
             TreeNode<Integer> currQ = queueQ.poll();
             if (currP.getVal() != currQ.getVal()) return false;
-            if (currP.getLeft() != null) queueP.add(currP.getLeft());
+            if (currP.getLeft() != null && currQ.getLeft() != null) {
+                queueP.add(currP.getLeft());
+                queueQ.add(currQ.getLeft());
+            }
+            if (currP.getRight() != null && currQ.getRight() != null) {
+                queueP.add(currP.getRight());
+                queueQ.add(currQ.getRight());
+            }
             if (currP.getLeft() == null && currQ.getLeft() != null) return false;
-            if (currP.getRight() != null) queueP.add(currP.getRight());
             if (currP.getRight() == null && currQ.getRight() != null) return false;
-
-            if (currQ.getLeft() != null) queueQ.add(currQ.getLeft());
-            if (currQ.getLeft() == null && currP.getLeft() != null) return false ;
-            if (currQ.getRight() != null) queueQ.add(currQ.getRight());
-            if (currQ.getRight() == null && currP.getRight() != null) return false ;
         }
-
-
         return true;
+    }
+
+    public boolean dfsSolution(TreeNode<Integer> p, TreeNode<Integer> q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.getVal() != q.getVal()) return false;
+        return dfsSolution(p.getLeft(), q.getLeft()) && dfsSolution(p.getRight(), q.getRight());
     }
 
     public static void main(String[] args) {
@@ -39,15 +46,15 @@ public class SameBinaryTrees {
         p1Root.setLeft(p1Node1);
         p1Root.setRight(p1Node2);
 
-        TreeNode<Integer> p2Root = new TreeNode<>(1);
-        TreeNode<Integer> p2Node1 = new TreeNode<>(2);
-        p2Root.setLeft(p2Node1);
-
         TreeNode<Integer> q1Root = new TreeNode<>(1);
         TreeNode<Integer> q1Node1 = new TreeNode<>(2);
         TreeNode<Integer> q1Node2 = new TreeNode<>(3);
         q1Root.setLeft(q1Node1);
         q1Root.setRight(q1Node2);
+
+        TreeNode<Integer> p2Root = new TreeNode<>(1);
+        TreeNode<Integer> p2Node1 = new TreeNode<>(2);
+        p2Root.setLeft(p2Node1);
 
         TreeNode<Integer> q2Root = new TreeNode<>(1);
         TreeNode<Integer> q2Node1 = new TreeNode<>(2);
@@ -68,6 +75,10 @@ public class SameBinaryTrees {
         System.out.println(new SameBinaryTrees().bfsSolution(p1Root, q1Root));
         System.out.println(new SameBinaryTrees().bfsSolution(p2Root, q2Root));
         System.out.println(new SameBinaryTrees().bfsSolution(p2Root, q2Root));
+        System.out.println("Second solution:");
+        System.out.println(new SameBinaryTrees().dfsSolution(p1Root, q1Root));
+        System.out.println(new SameBinaryTrees().dfsSolution(p2Root, q2Root));
+        System.out.println(new SameBinaryTrees().dfsSolution(p2Root, q2Root));
 
     }
 
