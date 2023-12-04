3d560946d3a87b32ef6c3cd9a621459131951dd5
Artuqu <percikartur@gmail.com>
2023-12-04 19:06:49 +0100

    Kth largest element in BST second solution

--- src/main/java/otherAlgorithms/binarySearch/LargestElementInBSTByK.java
+++ src/main/java/otherAlgorithms/binarySearch/LargestElementInBSTByK.java
@@ -1,8 +1,13 @@
 package otherAlgorithms.binarySearch;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LargestElementInBSTByK {
 
     private int counter = 0;
+    private int counterInt = 0;
+    private int result;
 
     public void solution(TreeNode<Integer> root, int k) {
         if (root == null || counter >= k) return;
@@ -11,6 +16,23 @@ public class LargestElementInBSTByK {
         if (counter == k) System.out.println(root.getVal());
         solution(root.getLeft(), k);
     }
+
+    public int solution2(TreeNode<Integer> root, int k) {
+        Deque<TreeNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode<Integer> curr = queue.poll();
+            if (curr.getRight() != null) solution2(curr.getRight(), k);
+            counterInt++;
+            if (counterInt == k) {
+                result = curr.getVal();
+                return result;
+            }
+            if (curr.getLeft() != null) solution2(curr.getLeft(), k);
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
 
@@ -33,7 +55,7 @@ public class LargestElementInBSTByK {
         firstNode4.setRight(firstNode7);
         firstNode5.setLeft(firstNode8);
         new LargestElementInBSTByK().solution(root, 6);
-
+        System.out.println(new LargestElementInBSTByK().solution2(root, 1));
 
     }
 }
