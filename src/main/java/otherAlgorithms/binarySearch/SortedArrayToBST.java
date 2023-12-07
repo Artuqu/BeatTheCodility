5be4f6df66f907c912196b6c30ba4af307d5038d
Artuqu <percikartur@gmail.com>
2023-12-07 19:01:39 +0100

    SortedArrayToBST add two extra ways of solution and printing the tree

--- src/main/java/otherAlgorithms/binarySearch/SortedArrayToBST.java
+++ src/main/java/otherAlgorithms/binarySearch/SortedArrayToBST.java
@@ -1,5 +1,7 @@
 package otherAlgorithms.binarySearch;
 
+import java.util.Random;
+
 public class SortedArrayToBST {
 
     public TreeNode<Integer> solution(int[] arr) {
@@ -9,17 +11,35 @@ public class SortedArrayToBST {
     private TreeNode<Integer> createMinimalBST(int[] arr, int left, int right) {
         if (left > right) return null;
 
+
+//        always left element
         int middle = (left + right) / 2;
+
+//        always right element
+//        if ((left + right) % 2 == 1) middle++;
+
+//        random element
+        if ((left + right) % 2 == 1) middle += new Random().nextInt(2);
+
+
         TreeNode<Integer> node = new TreeNode<>(arr[middle]);
         node.setLeft(createMinimalBST(arr, left, middle - 1));
         node.setRight(createMinimalBST(arr, middle + 1, right));
-        System.out.println(node.getVal());
         return node;
     }
 
     public static void main(String[] args) {
 
         int[] arr = {-3, 1, 0, 6, 9, 14, 22, 90};
-        new SortedArrayToBST().solution(arr);
+        TreeNode<Integer> solution = new SortedArrayToBST().solution(arr);
+        DepthFirstSearch ds = new DepthFirstSearch();
+        System.out.print("Preorder: ");
+        ds.preorder(solution);
+        System.out.println();
+        System.out.print("Postorder: ");
+        ds.postorder(solution);
+        System.out.println();
+        System.out.print("Inorder: ");
+        ds.inorder(solution);
     }
 }
