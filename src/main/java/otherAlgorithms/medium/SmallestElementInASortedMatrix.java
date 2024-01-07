567192a982b12742c6a7d8ee4d56c69b68aee0c1
Artuqu <percikartur@gmail.com>
2024-01-07 14:13:46 +0100

    SmallestElementInASortedMatrix time complexity O(n2) second solution

--- src/main/java/otherAlgorithms/medium/SmallestElementInASortedMatrix.java
+++ src/main/java/otherAlgorithms/medium/SmallestElementInASortedMatrix.java
@@ -1,6 +1,10 @@
 package otherAlgorithms.medium;
 
-public class SmallestElementInASortedMatrix {
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class SmallestElementInASortedMatrix implements Comparator<HeapNode> {
+    //    O(n)
     public int solution(int[][] matrix, int position) {
         for (int i = 0; i < matrix.length; i++) {
             if (position > matrix.length * i + matrix.length) continue;
@@ -11,11 +15,37 @@ public class SmallestElementInASortedMatrix {
         return -1;
     }
 
+    //O(n2)
+    public int solution2(int[][] matrix, int k) {
+        int N = matrix.length;
+        PriorityQueue<HeapNode> minHeap = new PriorityQueue<>(Math.min(N, k), new SmallestElementInASortedMatrix());
+        for (int r = 0; r < Math.min(N, k); r++) {
+            minHeap.offer(new HeapNode(matrix[r][0], r, 0));
+        }
+        HeapNode element = minHeap.peek();
+        while (k > 0) {
+            k--;
+            element = minHeap.poll();
+            int r = element.row, c = element.column;
+            if (c < N - 1) minHeap.offer(new HeapNode(matrix[r][c + 1], r, c + 1));
+        }
+        return element.value;
+    }
+
 
     public static void main(String[] args) {
-        int[][] matrix = {{1, 2, 3}, {5, 6, 7}, {9, 10, 21}};
+        int[][] matrix1 = {{-5}};
+        int[][] matrix3 = {{1, 2, 3}, {5, 6, 7}, {9, 10, 21}};
         int[][] matrix4 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 21, 32}, {33, 34, 46, 77}};
-        System.out.println(new SmallestElementInASortedMatrix().solution(matrix, 8));
+        System.out.println(new SmallestElementInASortedMatrix().solution(matrix1, 1));
+        System.out.println(new SmallestElementInASortedMatrix().solution(matrix3, 8));
+        System.out.println(new SmallestElementInASortedMatrix().solution2(matrix3, 8));
         System.out.println(new SmallestElementInASortedMatrix().solution(matrix4, 15));
+        System.out.println(new SmallestElementInASortedMatrix().solution2(matrix4, 15));
+    }
+
+    @Override
+    public int compare(HeapNode x, HeapNode y) {
+        return x.value - y.value;
     }
 }
