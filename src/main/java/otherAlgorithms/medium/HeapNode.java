5285c38161856bded85e885e4aebe481b983e6da
Artuqu <percikartur@gmail.com>
2024-01-08 12:02:02 +0100

    Heap Node class

---
+++ src/main/java/otherAlgorithms/medium/HeapNode.java
@@ -0,0 +1,12 @@
+package otherAlgorithms.medium;
+
+public class HeapNode {
+    public HeapNode(int value, int row, int column) {
+        this.value = value;
+        this.row = row;
+        this.column = column;
+    }
+    int row;
+    int column;
+    int value;
+}
