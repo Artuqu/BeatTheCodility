170a3529bb4b93dd5e49857cdac48db927a0e8fe
Artuqu <percikartur@gmail.com>
2024-01-08 13:59:51 +0100

    TopKFrequentElements second solution Map version

--- src/main/java/otherAlgorithms/medium/TopKFrequentElements.java
+++ src/main/java/otherAlgorithms/medium/TopKFrequentElements.java
@@ -1,8 +1,6 @@
 package otherAlgorithms.medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TopKFrequentElements {
 
@@ -25,10 +23,29 @@ public class TopKFrequentElements {
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
+    //using map solution
+    public int[] solution2(int[] arr, int k) {
+        Arrays.sort(arr);
+        Map<Integer, Integer> cache = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (cache.containsKey(arr[i])) cache.put(arr[i], cache.get(arr[i]) + 1);
+            else cache.put(arr[i], 1);
+        }
+        Set<Map.Entry<Integer, Integer>> entries = cache.entrySet();
+        for (Map.Entry<Integer, Integer> map : entries) {
+            if (map.getValue() >= k)
+                result.add(map.getKey());
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 2, 2, 3};
         int[] arr2 = {1};
         System.out.println(Arrays.toString(new TopKFrequentElements().solution(arr, 2)));
+        System.out.println(Arrays.toString(new TopKFrequentElements().solution2(arr, 2)));
         System.out.println(Arrays.toString(new TopKFrequentElements().solution(arr2, 1)));
+        System.out.println(Arrays.toString(new TopKFrequentElements().solution2(arr2, 1)));
     }
 }
