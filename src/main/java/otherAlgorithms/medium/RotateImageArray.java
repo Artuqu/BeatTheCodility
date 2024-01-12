9dcf99eb4a02a285bb90ad3acdde27cb3241acf7
Artuqu <percikartur@gmail.com>
2024-01-12 09:54:32 +0100

    RotateImageArray second solution

--- src/main/java/otherAlgorithms/medium/RotateImageArray.java
+++ src/main/java/otherAlgorithms/medium/RotateImageArray.java
@@ -17,11 +17,46 @@ public class RotateImageArray {
         return matrix;
     }
 
+    public void solution2(int[][] A) {
+        transpose(A);
+        reflect(A);
+    }
+
+    private void reflect(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = A[i][j];
+                A[i][j] = A[i][n - j - 1];
+                A[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    private void transpose(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = A[j][i];
+                A[j][i] = A[i][j];
+                A[i][j] = temp;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         int[][] arr = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        int[][] arrS = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
         int[][] arr2 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] arr2S = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
         System.out.println(Arrays.deepToString(new RotateImageArray().solution(arr)));
         System.out.println(Arrays.deepToString(new RotateImageArray().solution(arr2)));
+        ;
+        new RotateImageArray().solution2(arrS);
+        System.out.println(Arrays.deepToString(arrS));
+        new RotateImageArray().solution2(arr2S);
+        System.out.println(Arrays.deepToString(arr2S));
     }
 }
