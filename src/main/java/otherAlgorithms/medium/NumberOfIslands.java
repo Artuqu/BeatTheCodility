4cef0f5f13bb17a4e5603d41aac86f2f22713136
Artuqu <percikartur@gmail.com>
2024-01-12 13:51:40 +0100

    NumberOfIslands bfs solution

--- src/main/java/otherAlgorithms/medium/NumberOfIslands.java
+++ src/main/java/otherAlgorithms/medium/NumberOfIslands.java
@@ -1,8 +1,12 @@
 package otherAlgorithms.medium;
 
-public class NumberOfIslands {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class NumberOfIslands {
+    //DFS
     public int solution(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
         int n = grid.length;
         int m = grid[0].length;
         int counter = 0;
@@ -29,9 +33,42 @@ public class NumberOfIslands {
         dfs(grid, i, j - 1);
     }
 
+    //BFS
+    public int solution2(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        int counter = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    counter++;
+                    queue.add(new int[]{i, j});
+                    while (!queue.isEmpty()) {
+                        int[] curr = queue.poll();
+                        int first = curr[0];
+                        int second = curr[1];
+                        if (first >= n || first < 0 || second >= m || second < 0 || grid[first][second] != 1) continue;
+                        grid[first][second] = 0;
+                        for (int[] direction : directions) {
+                            int x = first + direction[0];
+                            int y = second + direction[1];
+                            queue.add(new int[]{x, y});
+                        }
+                    }
+                }
+            }
+        }
+
+        return counter;
+    }
 
     public static void main(String[] args) {
         int[][] grid = {{1, 1, 0, 0, 0}, {1, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 1}};
+        int[][] grid2 = {{1, 1, 0, 0, 0}, {1, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 1}};
         System.out.println(new NumberOfIslands().solution(grid));
+        System.out.println(new NumberOfIslands().solution2(grid2));
     }
 }
--- src/main/java/otherAlgorithms/medium/RotateImageArray.java
+++ src/main/java/otherAlgorithms/medium/RotateImageArray.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class RotateImageArray {
     public int[][] solution(int[][] matrix) {
         int n = matrix.length;
-        for (int i = 0; i < (n + 1) / 2; i++) {
-            for (int j = 0; j < n / 2; j++) {
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
                 int temp = matrix[n - 1 - j][i];
                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - i - 1];
@@ -46,7 +46,8 @@ public class RotateImageArray {
 
 
     public static void main(String[] args) {
-        int[][] arr = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        int[][] arr = new int[][]{{5, 1, 9, 11, 22}, {2, 4, 8, 10, 23}, {13, 3, 6, 7, 24}, {15, 14, 12, 16, 25},
+                {33, 34, 35, 36, 37}};
         int[][] arrS = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
         int[][] arr2 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] arr2S = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
