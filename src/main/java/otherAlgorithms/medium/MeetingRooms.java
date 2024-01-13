6540596c855eabc646652381dec3003adfd68e29
Artuqu <percikartur@gmail.com>
2024-01-13 16:47:36 +0100

    MeetingRooms second solution (correct)

--- src/main/java/otherAlgorithms/medium/MeetingRooms.java
+++ src/main/java/otherAlgorithms/medium/MeetingRooms.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class MeetingRooms {
 
-
+    // solution with bug
     public int solution(int[][] intervals) {
         if (intervals.length == 0 || intervals[0].length == 0) return 0;
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
@@ -19,21 +19,39 @@ public class MeetingRooms {
             queue.add(intervals[i]);
             i++;
         }
-
         return counter;
     }
 
+    public int solution2(int[][] intervals) {
+        if (intervals.length == 0 || intervals[0].length == 0) return 0;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<Integer> endTimes = new PriorityQueue<>();
+        endTimes.offer(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            int earliestEndTime = endTimes.peek();
+            if (interval[0] >= earliestEndTime) endTimes.poll();
+            endTimes.offer(interval[1]);
+        }
+
+        return endTimes.size();
+    }
 
     public static void main(String[] args) {
         int[][] arr = {{0, 30}, {5, 10}, {15, 20}};
         int[][] arr2 = {{7, 10}, {2, 4}};
         int[][] arr3 = {{1, 10}, {2, 7}, {3, 19}, {8, 12}, {10, 20}, {11, 30}};
-        int[][] arr4 = {{}, {}};
+        int[][] arr4 = {{1, 2}};
 
         System.out.println(new MeetingRooms().solution(arr));
         System.out.println(new MeetingRooms().solution(arr2));
         System.out.println(new MeetingRooms().solution(arr3));
         System.out.println(new MeetingRooms().solution(arr4));
+        System.out.println();
+        System.out.println(new MeetingRooms().solution2(arr));
+        System.out.println(new MeetingRooms().solution2(arr2));
+        System.out.println(new MeetingRooms().solution2(arr3));
+        System.out.println(new MeetingRooms().solution2(arr4));
 
     }
 }
