54983e6f51eacb8e95227caf2a8192b3d7294197
Artuqu <percikartur@gmail.com>
2024-01-16 21:37:03 +0100

    Coin Change O(N) solution

--- src/main/java/otherAlgorithms/medium/CoinChange.java
+++ src/main/java/otherAlgorithms/medium/CoinChange.java
@@ -1,5 +1,7 @@
 package otherAlgorithms.medium;
 
+import java.util.Arrays;
+
 public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
@@ -16,9 +18,24 @@ public class CoinChange {
         return (minCoins == Integer.MAX_VALUE) ? -1 : minCoins;
     }
 
+    public int coinChange2(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int counter = 0;
+        for (int i = coins.length - 1; i >= 0; i--) {
+            while (amount >= coins[i]) {
+                amount -= coins[i];
+                counter++;
+            }
+        }
+        return amount == 0 ? counter : -1;
+    }
+
 
     public static void main(String[] args) {
         int[] coins = {1, 2, 5};
         System.out.println(new CoinChange().coinChange(coins, 11));
+        System.out.println(new CoinChange().coinChange2(coins, 11));
+
     }
+
 }
