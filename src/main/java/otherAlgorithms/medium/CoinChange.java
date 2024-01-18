bd29dd8d9080f5449ef1a8bec076eb98c710bfb6
Artuqu <percikartur@gmail.com>
2024-01-18 11:18:06 +0100

    Coin Change Memoization solution

--- src/main/java/otherAlgorithms/medium/CoinChange.java
+++ src/main/java/otherAlgorithms/medium/CoinChange.java
@@ -30,11 +30,32 @@ public class CoinChange {
         return amount == 0 ? counter : -1;
     }
 
+//    Memoization
+    public int coinChange3(int[] coins, int amount) {
+        return coinChange3(coins, amount, new int[amount]);
+    }
+
+    private int coinChange3(int[] coins, int rem, int[] count) {
+        if (rem == 0) return 0;
+        if (rem < 0) return -1;
+        if (count[rem - 1] != 0) return count[rem - 1];
+        int minCoins = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int numCoins = coinChange3(coins, rem - coin, count);
+            if (numCoins != -1) {
+                minCoins = Math.min(minCoins, numCoins + 1);
+            }
+        }
+        count[rem - 1] = (minCoins == Integer.MAX_VALUE) ? -1 : minCoins;
+        return count[rem - 1];
+    }
+
 
     public static void main(String[] args) {
         int[] coins = {1, 2, 5};
         System.out.println(new CoinChange().coinChange(coins, 11));
         System.out.println(new CoinChange().coinChange2(coins, 11));
+        System.out.println(new CoinChange().coinChange3(coins, 11));
 
     }
 
